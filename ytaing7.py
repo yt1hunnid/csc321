d62c8236507c0fb2096159cd439d7edc3617f236
Yong <yongtaing12@gmail.com>
2024-10-23 01:25:58 +0000

    lab 7

---
+++ ytaing7.py
@@ -0,0 +1,31 @@
+# YYong Taing 
+# CSC 321
+# Lab 7
+
+x = 12 
+y = 0.6
+z = "Granola"
+
+if x < 11:
+    print("x is less than 11")
+elif x == 12:
+    print("x is 12")
+elif x > 13:
+    print("x is greater than 13")
+else:
+    print("x is between 11 and 13 but not 12")
+
+if y < 0:
+    print("y is less than 0")
+elif y == 0.6:
+    print("y is 0.6")
+elif y > 1:
+    print("y is greater than 1")
+else:
+    print("y is between 0 and 1 but not 0.6")
+
+if z == "Granola":
+    print("z is Granola")
+else:
+    print("z is not Granola")
+
